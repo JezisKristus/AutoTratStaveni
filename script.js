41a2055c9b5b926acb5b96cc453179c6773bcc20
vlasakantonin <vlasaktonda11@gmail.com>
2026-02-09 09:33:21 +0100

    Added images to grid and made it work together, next stem is saving and loading

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const grid = document.getElementById('grid');
 const homeMenu = document.getElementById('home');
 const editor = document.getElementById('editor');
 const btnNew = document.getElementById('newMap');
-
+const btnBackHome = document.getElementById('backHome');
+const btnClear = document.getElementById('clear');
 
 editor.classList.add('hidden');
+
+btnClear.addEventListener('click', () => {
+    for(let i = 0; i < 400; i++) {
+       grid.children[i].dataset.state = 0;
+    }
+})
 
 btnNew.addEventListener('click', (e) => {
     e.preventDefault();
@@ -14,8 +21,23 @@ btnNew.addEventListener('click', (e) => {
     homeMenu.classList.add('hidden');
 })
 
+btnBackHome.addEventListener('click', (e) => {
+    e.preventDefault();
+    homeMenu.classList.remove('hidden');
+    editor.classList.add('hidden');
+})
+
 for(let i = 0; i < 400; i++) {
     const cell = document.createElement("span");
+    cell.classList.add('cell');
+    cell.dataset.state = 0;
+
     grid.appendChild(cell);
 }
+
+grid.addEventListener("click", e => {
+    if (!e.target.classList.contains("cell")) return;
+    e.target.dataset.state =
+        (Number(e.target.dataset.state) + 1) % 4;
+});
 
